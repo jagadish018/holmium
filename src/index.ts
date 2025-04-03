8109064a6ccfc9dec36e2e0b16ece8041b69540f
jagadishck018 <jagadishck.018@gmail.com>
2025-04-03 14:23:04 +0530

    add college endpoint to return college information

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,15 @@ app.get("/platform", (c) => {
   return c.json({
     platform: process.platform
   })
+})
+  
+app.get("/college", (c) => {
+  return c.json({
+    college: "Siddagnaga Institute of technology",
+    address: "BH road tumkur",
+    pincode: "572103"
   })
+})
 serve({
   fetch: app.fetch,
   port: 3000
